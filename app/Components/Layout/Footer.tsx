4332d830100652b55652f374fe95437516abedce
dotmantosh <dotuntosh@gmail.com>
2024-02-15 18:19:59 +0100

    Home page completed

--- app/Components/Layout/Footer.tsx
+++ app/Components/Layout/Footer.tsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import styles from '../../Styles/_footer.module.css'
+import { GithubIconSvgLight, LinkedinIconSvgLight, TwitterIconSvgLight } from '../SVGs/SVGIcons'
 
 function AppFooter() {
   return (
-    <div>
-      Footer
-    </div>
+    <footer className={styles.footer}>
+      <div className={styles.footerTop}>
+        <div className={styles.logo}>
+          {`{{ M P W }}`}
+        </div>
+
+        <div className={styles.footerTopRight}>
+          <p className='mb-0'>0808080808</p>
+          <p className='mb-0'>info@example.com</p>
+
+          <div className={styles.footerSocials}>
+            <GithubIconSvgLight />
+            <TwitterIconSvgLight />
+            <LinkedinIconSvgLight />
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.footerBottom}>
+        <ul className={styles.footerNav}>
+          <li>Home</li>
+          <li>About</li>
+          <li>Tech Stack</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </footer>
   )
 }
 
