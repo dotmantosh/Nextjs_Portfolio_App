7896dd7e982fb0604618b51f80830c81ab2f39fa
dotmantosh <dotuntosh@gmail.com>
2024-04-03 14:21:49 +0100

    layout components

--- app/Components/Layout/AppLayout.tsx
+++ app/Components/Layout/AppLayout.tsx
@@ -4,6 +4,8 @@ import { Container } from 'reactstrap'
 import Head from 'next/head'
 import AppHeader from './AppHeader'
 import AppFooter from './Footer'
+import NextTopLoader from "nextjs-toploader";
+import { Toaster } from 'sonner'
 
 
 interface LayoutProps {
@@ -16,6 +18,27 @@ const AppLayout: FunctionComponent<LayoutProps> = ({ children }): ReactElement =
       <Head>
         <title>My Portfolio Website</title>
       </Head>
+      <NextTopLoader
+        // color="#E96209"
+        color="#018c0f"
+        initialPosition={0.08}
+        crawlSpeed={200}
+        height={3}
+        crawl={true}
+        showSpinner={true}
+        easing="ease"
+        speed={200}
+        shadow="0 0 10px #f7a738,0 0 5px #b75412"
+      />
+      <Toaster
+        position="bottom-center"
+        richColors
+        closeButton
+        toastOptions={{
+          duration: 3000,
+          unstyled: false,
+        }}
+      />
       <Container>
         <AppHeader />
         {children}
