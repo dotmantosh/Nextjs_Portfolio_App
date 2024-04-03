db2ef047273455629b2ec343cf3154aa970b611e
dotmantosh <dotuntosh@gmail.com>
2024-04-03 14:20:53 +0100

    api calls setup

---
+++ app/api/apiRoutes.ts
@@ -0,0 +1,7 @@
+export class ApiRoutes {
+  static BASE_URL_DEV = "http://localhost:5000/api/v1";
+
+  static SignUpUser = "/user/signup";
+  static LoginUser = "/user/login";
+  static LogoutUser = "/user/logout";
+}
