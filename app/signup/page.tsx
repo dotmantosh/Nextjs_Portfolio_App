383be8276bfdecf356a97014f594759c0e055edd
dotmantosh <dotuntosh@gmail.com>
2024-04-27 09:11:46 +0100

    Authentication pages

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,6 +1,10 @@
 import SignUp from "./Signup";
-
-export default function Page() {
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+export default async function Page() {
+  const session = await getServerSession(authOptions)
+  if (session) redirect(`/profile/${session?.user?.username}`)
   return (
     <SignUp />
   )
